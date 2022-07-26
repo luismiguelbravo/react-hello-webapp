979ab6e7ba48657aa6c6d5a2abd383369b30828c
Luis Miguel Bravo <lmbgott@gmail.com>
2022-07-26 18:40:20 -0400

    add style on navbar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -6,18 +6,21 @@ import "../../styles/navbar.css";
 export const Navbar = () => {
 	return (
     <nav className="nav-bar">
-      <Link className="no-decoration" to="/">
-        <span className="navbar-brand mb-0">Home</span>
-      </Link>
-      <Link className="no-decoration" to="/characters">
-        <span className="navbar-brand mb-1">characters</span>
-      </Link>
-      <Link className="no-decoration" to="/vehicles">
-        <span className="navbar-brand mb-1">vehicles</span>
-      </Link>
-      <Link className="no-decoration" to="/planets">
-        <span className="navbar-brand mb-1">planets</span>
-      </Link>
+      <div className='container'>
+        <Link className="no-decoration" to="/">
+          <span className="navbar-brand mb-0">Home</span>
+        </Link>
+        <Link className="no-decoration" to="/characters">
+          <span className="navbar-brand mb-1">Characters</span>
+        </Link>
+        <Link className="no-decoration" to="/vehicles">
+          <span className="navbar-brand mb-1">Vehicles</span>
+        </Link>
+        <Link className="no-decoration" to="/planets">
+          <span className="navbar-brand mb-1">Planets</span>
+        </Link>
+      </div>
+
     </nav>
 	);
 };
