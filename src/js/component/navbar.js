dc450b8faa3d6a2307702f7a7151c5986096456b
Luis Miguel Bravo <lbravo@myhotel.com.es>
2022-07-25 21:39:13 -0400

    add style to nav bar

--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -5,13 +5,13 @@ import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav className="nav-bar">
-			<Link className="no-decoration " to="/">
-				<span className="navbar-brand mb-0">Home</span>
-			</Link>
-      <Link to="/characters">
-				<span className="navbar-brand mb-1">characters</span>
-			</Link>
-		</nav>
+    <nav className="nav-bar">
+      <Link className="no-decoration" to="/">
+        <span className="navbar-brand mb-0">Home</span>
+      </Link>
+      <Link className="no-decoration" to="/characters">
+        <span className="navbar-brand mb-1">characters</span>
+      </Link>
+    </nav>
 	);
 };
