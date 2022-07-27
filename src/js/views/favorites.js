5b0a3984c5af0fd1880b2cbfde6aa9ee6fdac53d
Luis Miguel Bravo <lmbgott@gmail.com>
2022-07-26 22:32:32 -0400

    ser responsive style for favorites

--- src/js/views/favorites.js
+++ src/js/views/favorites.js
@@ -1,13 +1,10 @@
 import React, { useState, useEffect, useContext } from "react";
 import { Link } from "react-router-dom";
-
-import { Context } from "../store/appContext";
+import "../../styles/favorites.css";
 
 export const Favorites = () => {
 
   const [favoriteList, setFavoriteList] = useState(false)
-
-	const { store, actions } = useContext(Context);
 
   const loadFavoriteList = () => {
     setFavoriteList(JSON.parse(localStorage.getItem('favoriteList')));
@@ -22,33 +19,25 @@ export const Favorites = () => {
 
       <h1>Favorites</h1>
 
-      <div className="row">
-        <div className="col-12 col-sm-6 col-md-6">
-          <img className="character-detail-img" src={".jpg"}></img>
-        </div>
-        <div className="col-12 col-sm-6 col-md-6">
-
-        </div>
-      </div>
-
-
-			<ul className="list-group">
+			<div className="row">
 				{!favoriteList ? <div>empty</div> : favoriteList.map((favorite, index) => {
 					return (
-						<li key={index} className="list-group-item d-flex justify-content-between">
-              <Link to={favorite.url}>
-                <span>{favorite.name}</span>
-                <img className="character-detail-img" src={favorite.image}></img>
-                <button className="btn btn-warning" onClick={() => actions.changeColor(index, "orange")}>
-								remove 🗑️
-							</button>
-              </Link>
-              
-							
-						</li>
+            <div className="col-6 col-sm-6 col-md-4 col-lg-3	" key={index}>
+              <div className="favorite-thumbnails">
+              <Link className="favorite-no-text-decoration" to={favorite.url}>
+                  <div className="image-container">
+                    <img className="favorite-image" src={favorite.image}></img>
+                  </div>
+                  <div className="favorite-name">{favorite.name}</div>
+                </Link>
+                <button className="remove-button btn btn-warning" onClick={() => actions.changeColor(index, "orange")}>
+                  remove 🗑️
+                </button>
+              </div>
+            </div>
 					);
 				})}
-			</ul>
+			</div>
 
 		</div>
 	);
